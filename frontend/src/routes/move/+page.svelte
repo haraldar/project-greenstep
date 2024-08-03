256285d48f5ea26e5905ec755eaebbb0d8590f28
nikitakhutorni <nkhutorni@protonmail.com>
2024-08-03 11:46:28 +0200

    initial frontend

---
+++ frontend/src/routes/move/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { minsToMeterRadius } from '$lib/utils';
+	import { Button } from '$lib/components/ui/button';
+	import Map from '$lib/components/Map.svelte';
+</script>
+
+<div class="flex h-full w-full flex-grow flex-col items-center justify-center">
+	<div class="pb-2 text-center text-2xl font-bold">Move a little!</div>
+	<div class="">
+		<Map
+			radius={minsToMeterRadius(49, 15)}
+			categoryFilter="kultur sehenswürdigkeiten,anderes/parks und grünanlagen,anderes/stadtrundgang"
+		/>
+	</div>
+	<div class="pt-5"><Button href="/">Next</Button></div>
+</div>
